b0762342f018f88a05c8b0fc8a228eb04d67b67a
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-28 10:23:34 +0200

    Brew formula update for kratos version v0.8.0-alpha.1

--- kratos.rb
+++ kratos.rb
@@ -5,28 +5,35 @@
 class Kratos < Formula
   desc ""
   homepage "https://www.ory.sh"
-  version "0.8.0-alpha.1.pre.3"
-  bottle :unneeded
+  version "0.8.0-alpha.1"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/ory/kratos/releases/download/v0.8.0-alpha.1.pre.3/kratos_0.8.0-alpha.1.pre.3-sqlite_macos_64bit.tar.gz"
-      sha256 "3fcb1981090abedca048ba4720c0f2da0513f8807e4a0f90f7a729b569935686"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/ory/kratos/releases/download/v0.8.0-alpha.1.pre.3/kratos_0.8.0-alpha.1.pre.3-sqlite_macos_arm64.tar.gz"
-      sha256 "9bd31e9888d2b82d9e63a6f216868cccd9cd8daed233de70ed53b06dfb35c8b4"
+      url "https://github.com/ory/kratos/releases/download/v0.8.0-alpha.1/kratos_0.8.0-alpha.1-sqlite_macos_arm64.tar.gz"
+      sha256 "240010faf14174c35517e7d92652d7ed1686632f0f0f6cb9094c9b677752d45a"
+
+      def install
+        bin.install "kratos"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ory/kratos/releases/download/v0.8.0-alpha.1/kratos_0.8.0-alpha.1-sqlite_macos_64bit.tar.gz"
+      sha256 "d93393306a402a67d24357f95b637cdc21b9df560e071f03a22d1bcbac91170a"
+
+      def install
+        bin.install "kratos"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ory/kratos/releases/download/v0.8.0-alpha.1.pre.3/kratos_0.8.0-alpha.1.pre.3-sqlite_linux_64bit.tar.gz"
-      sha256 "21179cdd583edf639a44c520dd6365f6c6045064764a8512cc05514f5ccf4989"
-    end
-  end
+      url "https://github.com/ory/kratos/releases/download/v0.8.0-alpha.1/kratos_0.8.0-alpha.1-sqlite_linux_64bit.tar.gz"
+      sha256 "617f570f41ab183340daa4468cc558e388b6d3c714f7491b88c63972d15d920d"
 
-  def install
-    bin.install "kratos"
+      def install
+        bin.install "kratos"
+      end
+    end
   end
 end
